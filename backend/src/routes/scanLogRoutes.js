8894ce08f2425a623129eee006c821804d8640df
Fida Taskeen <fidataskeen247@gmail.com>
2026-08-18 10:11:07 +0530

    remove dead deleteScanLog route causing startup crash

--- backend/src/routes/scanLogRoutes.js
+++ backend/src/routes/scanLogRoutes.js
@@ -1,14 +1,10 @@
 const express = require('express');
 const router = express.Router();
-const { getJobOrderStatus, addScan, getScanLogs, getMyStats, deleteScanLog } = require('../controllers/scanLogController');
+const { getJobOrderStatus, addScan, getScanLogs, getMyStats } = require('../controllers/scanLogController');
 const { protect, authorize } = require('../middleware/auth');
-
 router.use(protect);
-
 router.get('/my-stats', getMyStats);
 router.get('/job-order-status/:jobOrderId', getJobOrderStatus);
 router.get('/', getScanLogs);
 router.post('/', addScan);
-router.delete('/:id', authorize('admin'), deleteScanLog);
-
 module.exports = router;
